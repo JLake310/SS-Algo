5be0fa3949222e91d3da46eb3661f64d5c83ca47
JLake310 <86578246+JLake310@users.noreply.github.com>
2022-01-13 11:03:25 +0900

    Update BJ1932_정수삼각형.cpp

--- BJ-PS/동적계획법1/BJ1932_정수삼각형.cpp
+++ BJ-PS/동적계획법1/BJ1932_정수삼각형.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-int tri[500][500];
-int dp[500][500];
 int N;
+int map[500][500];
+int dp[500][500];
+int answer;
 
 int main(int argc, char** argv) {
-	
-	scanf("%d", &N);
-	
-	for(int i=0;i<N;i++){
-		for(int j=0; j<=i; j++){
-			scanf("%d", &tri[i][j]);
-		}
-	}
-	
-	dp[0][0] = tri[0][0];
-	
-	for(int i=0; i<N; i++){
-		for(int j=0; j<=i; j++){
-			if(j == 0){
-				dp[i][j] = tri[i][j] + dp[i-1][j];
-			}
-			else if(j == i){
-				dp[i][j] = tri[i][j] + dp[i-1][j-1];
-			}
-			else{
-				dp[i][j] = tri[i][j] + max(dp[i-1][j], dp[i-1][j-1]);
-			}
-		}
-	}
-	
-	int answer=0;
-	
-	for(int i=0;i<N; i++){
-		if(answer < dp[N-1][i]) answer = dp[N-1][i];
-	}
-	
-	printf("%d", answer);
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            scanf("%d", &map[i][j]);
+        }
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    dp[0][0] = map[0][0];
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0) {
+                dp[i][j] = map[i][j] + dp[i - 1][j];
+            }
+            else if (j == i) {
+                dp[i][j] = map[i][j] + dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = map[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1]);
+            }
+        }
+    }
+
+    answer = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (dp[N - 1][i] > answer) answer = dp[N - 1][i];
+    }
+
+    printf("%d\n", answer);
 
     return 0;
 }
